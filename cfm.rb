88514363ad9bb303e29bec3d48eb1a27626b223b
Nguyen Duc Giang <ndgiang84@gmail.com>
2011-03-18 16:24:00 +0700

    Added Facebook app_id, app_secret.

---
+++ cfm.rb
@@ -0,0 +1,45 @@
+#require 'omniauth/oauth'
+
+#enable :sessions
+
+#Here you have to put your own Application ID and Secret
+APP_ID = "171689842883457"
+APP_SECRET = "47e8fda6e48f7ddacafdb70707327b97"
+
+=begin
+use OmniAuth::Builder do
+  provider :facebook, APP_ID, APP_SECRET, { :scope => 'email, status_update, publish_stream' }
+end
+
+get '/' do
+  @articles = []
+  @articles << {:title => 'Deploying Rack-based apps in Heroku', :url => 'http://docs.heroku.com/rack'}
+  @articles << {:title => 'Learn Ruby in twenty minutes', :url => 'http://www.ruby-lang.org/en/documentation/quickstart/'}
+
+  erb :index
+end
+
+get '/auth/facebook/callback' do
+  session['fb_auth'] = request.env['omniauth.auth']
+  session['fb_token'] = session['fb_auth']['credentials']['token']
+  session['fb_error'] = nil
+  redirect '/'
+end
+
+get '/auth/failure' do
+  clear_session
+  session['fb_error'] = 'In order to use this site you must allow us access to your Facebook data<br />'
+  redirect '/'
+end
+
+get '/logout' do
+  clear_session
+  redirect '/'
+end
+
+def clear_session
+  session['fb_auth'] = nil
+  session['fb_token'] = nil
+  session['fb_error'] = nil
+end
+=end
